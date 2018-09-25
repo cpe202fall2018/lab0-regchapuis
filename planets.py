0d4956bf460ccad4c05beaba8d2d4e4085a20c5b
regchapuis <43481624+regchapuis@users.noreply.github.com>
2018-09-24 23:08:24 -0700

    Update planets.py
    This is my final revision of lab 0 and this time there's a space after the question mark.

--- planets.py
+++ planets.py
@@ -1,9 +1,8 @@
 def weight_on_planets():
-   earthstr = input("What do you weigh on earth?")
-   ear = int(earthstr)
-   mars = ear*0.38
-   jup = ear*2.34
-   print("\nOn Mars you would weigh %5.2f pounds.\nOn Jupiter you would weigh %6.2f pounds." %(mars, jup))
+    ear = int(input("What do you weigh on earth? "))
+    mars = ear*0.38
+    jup = ear*2.34
+    print("\nOn Mars you would weigh %.2f pounds.\nOn Jupiter you would weigh %.2f pounds." %(mars, jup))
    
    
    
